3dfd716e5d56489aa28a998408ef23b42569a798
Hillel Antonov <dev.antonov.hillel@dev.bwater.com>
2019-11-10 19:40:14 -0500

    Add flow dialog functionality
    There is an issue with validation that doesn't work properly

---
+++ data_models/user_profile.py
@@ -0,0 +1,10 @@
+"""
+  This is our application state. Just a regular serializable Python class.
+"""
+
+
+class UserProfile:
+    def __init__(self, name: str = None, transport: str = None, age: int = 0):
+        self.name = name
+        self.transport = transport
+        self.age = age
